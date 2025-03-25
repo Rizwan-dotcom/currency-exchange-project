64060f3e9bc785ce22bc43e0465ea09201273d85
Rizwan-dotcom <mrizwanhafeez313@gmail.com>
2025-03-25 13:16:33 +0500

    App Css file delete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Sidebar } from './component/sidebar';
 function App() {
   return (
     <div className="App">
-     <h2>Hello boys</h2>
+    <Sidebar/>
     </div>
   );
 }
--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='bg-white shadow-lg ml-3 mt-2'>
+        <div>
+
+        </div>
+    </div>
   )
 }
