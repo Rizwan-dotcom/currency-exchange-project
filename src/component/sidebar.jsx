0863d3979079d90de64ce57bf2c11ec294a1f4f7
Rizwan-dotcom <mrizwanhafeez313@gmail.com>
2025-03-25 14:52:42 +0500

    Updated Sidebar component

--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 export const Sidebar = () => {
   return (
-    <aside className='bg-white shadow-md ml-3 mt-2'>
-        <div className='p-3'>
-        <img src="/images/logo.png" alt="logo" className='w-5 px-2'/>
-        <span>dailyexchanger Dashboard</span>
-        </div>
+    <aside className='fixed top-50 left-4 w-[280px] h-[100vh] rounded-lg shadow-md mt-5'>
+      <div className="flex items-center h-20 space-x-2 pl-8 py-5">
+  <img src="/images/logo.png" alt="Logo" className="w-8 h-5 mt-1" />
+  <h1 className="text-md font-medium">dailyexchanger Dashboard</h1>
+</div>
+<hr className="bg-gradient-to-r from-gray-200 via-gray-400 to-gray-200 h-[1px]"/>
+<nav>
+  <ul><li>
+    <a href="">
+     
+    </a>
+    </li></ul>
+</nav>
     </aside>
   )
 }
